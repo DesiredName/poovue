a10ebfb01878a3b5de5c88986f05b6b4faaeb839
DesiredName <wc3-enter@yandex.ru>
2025-05-31 15:55:42 +0200

    'update'

--- server/users.table.ts
+++ server/users.table.ts
@@ -1,6 +1,7 @@
 const h1: UserHighlight =  {
     avatar: '/users/00123-233-aa345/th-17235274.jpg',
     backgroundImage: '/users/00123-233-aa345/OIP-2375605357.jpg',
+    discoverImage: '/users/96730-022-ab4dc/OIP-2661069431.jpg.jpg',
     nickname: 'bad_man',
     username: 'BadMan',
 };
@@ -8,6 +9,7 @@ const h1: UserHighlight =  {
 const h2: UserHighlight =  {
     avatar: '/users/02302-112-ee93d/th-3435364780.jpg',
     backgroundImage: '/users/02302-112-ee93d/OIP-1654321177.jpg',
+    discoverImage: '/users/96730-022-ab4dc/th-1650011434.jpg',
     nickname: 'mm',
     username: 'Mishaka Milashka',
 };
@@ -15,6 +17,7 @@ const h2: UserHighlight =  {
 const h3: UserHighlight =  {
     avatar: '/users/11534-000-aef2d/th-2414990418.jpg',
     backgroundImage: '/users/11534-000-aef2d/th-231068622.jpg',
+    discoverImage: '/users/96730-022-ab4dc/th-2839540026.jpg',
     nickname: 'Little Smile',
     username: 'lsm',
 };
@@ -22,22 +25,9 @@ const h3: UserHighlight =  {
 const h4: UserHighlight =  {
     avatar: '/users/72835-025-ab45d/th-1880575820.jpg',
     backgroundImage: '/users/72835-025-ab45d/th-2371672902.jpg',
+    discoverImage: '/users/96730-022-ab4dc/th-2354096698.jpg',
     nickname: 'Vanilla',
     username: 'vanilla 1993 ',
-};
-
-const h5: UserHighlight =  {
-    avatar: '/users/94754-366-7a43e/th-751499544.jpg',
-    backgroundImage: '/users/94754-366-7a43e/th-1657683960.jpg',
-    nickname: 'JohannaM',
-    username: 'morava_mountain',
-};
-
-const h6: UserHighlight =  {
-    avatar: '/users/96730-022-ab4dc/th-4066389066.jpg',
-    backgroundImage: '/users/96730-022-ab4dc/th-867891366.jpg',
-    nickname: 'ISU',
-    username: 'isu',
 };
 
 export const me: Me = {
@@ -48,4 +38,19 @@ export const me: Me = {
     isUnsafeContentAllowed: true,
 };
 
+const h5: UserHighlight =  {
+    avatar: '/users/94754-366-7a43e/th-751499544.jpg',
+    backgroundImage: '/users/94754-366-7a43e/th-1657683960.jpg',
+    discoverImage: '/users/96730-022-ab4dc/th-3325837478.jpg',
+    nickname: 'JohannaM',
+    username: 'morava_mountain',
+};
+
+const h6: UserHighlight =  {
+    avatar: '/users/96730-022-ab4dc/th-4066389066.jpg',
+    backgroundImage: '/users/96730-022-ab4dc/th-867891366.jpg',
+    discoverImage: '/users/96730-022-ab4dc/OIP-865355825.jpg',
+    nickname: 'ISU',
+    username: 'isu',
+};
 export default [h1,h2,h3,h4,h5,h6];
--- types/user_hightlight.d.ts
+++ types/user_hightlight.d.ts
@@ -1,6 +1,7 @@
 declare type UserHighlight = {
     avatar: string;
     backgroundImage: string;
+    discoverImage: string;
     username: string;
     nickname: string;
 };
